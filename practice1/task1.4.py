077198419f9fc4b9af57daf3f3ef44b205201702
Lvov Tinley <ffff@yandex.ru>
2021-02-15 14:35:06 +0300

    fixed task1.4

--- practice1/task1.4.py
+++ practice1/task1.4.py
@@ -1,13 +1,16 @@
 import math
 
 
-def f14(n):
+def count(n):
     if (n == 0):
         return 2
     elif (n == 1):
         return 2
     else:
-        return (math.pow(f14(n - 1), 2) / 87) - abs(f14(n - 1))
+        return (math.pow(count(n - 1), 2) / 87) - abs(count(n - 1))
+
+def f14(n):
+    return "{:.2e}".format(count(n))
 
 
-#print("{:.2e}".format(f14(13)))
+print(f14(13))
