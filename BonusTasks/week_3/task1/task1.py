1f7bfe2d58ca22116f1aaa06726f5f1bd50511ea
LvovTR <lvovtr@yandex.ru>
2021-03-01 13:12:42 +0300

    fixed pr21

--- BonusTasks/week_3/task1/task1.py
+++ BonusTasks/week_3/task1/task1.py
@@ -1,37 +1,37 @@
 #task 1
 """
-s = ['235', '1223', '228']
+s = ['23', '123', '1337']
 a = [int(i) for i in s]
 print(s)
 print(a)
 """
 #task 2
 """
-s = ['235', '1223', '228', '228']
+s = ['23', '123', '1337', '1337']
 a = [len(set(s))]
 print(a)
 """
 #task 3
 """
-s = ['235', '1223', '228', '228']
+s = ['23', '123', '1337', '1337']
 a = s[::-1]
 print(a)
 """
 #task 4
 """
-s = ['235', '1223', '228', '228']
-a = [i for i, j in enumerate(s) if j == '228']
+s = ['23', '123', '1337', '1337']
+a = [i for i, j in enumerate(s) if j == '1337']
 print(a)
 """
 #task 5
 """
-s = [235, 1223, 228, 228, 1992]
+s = [23, 123, 1337, 1337, 1992]
 a = sum([j for i, j in enumerate(s) if not i % 2])
 print(a)
 """
 #task 6
 """
-s = ['235', '1223', '228', '228', 'VeryLongStr']
+s = ['23', '123', '1337', '1337', 'VeryLongStr']
 a = max([len(i) for i in s])
 print(a)
 """
